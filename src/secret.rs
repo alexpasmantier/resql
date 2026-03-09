1a37d8a5c83e757943fe2be8ca6fad46b6334959
Alex Pasmantier <alexandre.pasmantier@gitguardian.com>
2026-03-09 17:38:50 +0100

    update the api key

--- src/secret.rs
+++ src/secret.rs
@@ -1,3 +1,3 @@
-const my_api_key: &str = "sk_sadfaoliu5234mkldf2309u324m";
+const api_key: &str = "sgp_3a99Ee8086BBc8C7d08c841c4e211CeaecF150ac";
 
 const password: &str = "my_super_secret_password";
